f7c29edef25de271e6ee761706acc802e49dbff0
nealgarcia <nathnlgarcia@gmail.com>
2022-01-11 09:27:46 -0600

    make models and migrations

--- nostaldja/models.py
+++ nostaldja/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Decade(models.Model):
+    start_year = models.DateField()
+
+    def __str__(self):
+        return self.start_year
+
+class Fads(models.Model):
+    name = models.CharField(max_length = 100)
+    image_url = models.CharField(max_length = 150)
+    description = models.TextField()
+    decade = models.DateField()
+
+    def __str__(self):
+        return self.name
